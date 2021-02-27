f839149782a7261310ff7d8f65495f6f3a8e0d2e
piyush-soni <42142059+piyush-soni@users.noreply.github.com>
2021-02-27 11:27:13 +0530

    added the code for connections list , tested the code and it works perfectly

--- DDA.py
+++ DDA.py
@@ -15,7 +15,10 @@ def checkstatus():
     #         status[i] = 1
     print("enter the device number between 0 to",num_of_devices," and enter the status by 1 or 0 , EG: 1 1 :")
     n, s = map(int, input().split())
-    status[n] = s
+    if(n > num_of_devices or n<0 or n!=int(n)):
+        print("invalid device number")
+    else:
+        status[n] = s
 
 def primes_from_2_to(n):
    
@@ -59,26 +62,36 @@ num_of_devices = int(input("Number of devices: "))
 GridDimeniton = int(input("Grid dimention :"))
 a = halton(2, num_of_devices)*GridDimeniton
 
-connections = []
-for i in range(num_of_devices):
-    connections.append([])
-
 x,y,status = [],[],[]
 for i in a:
 	x.append(i[0])
 	y.append(i[1])
 	status.append(1)
 
+radius = 20
+connections = []
+for i in range(num_of_devices):
+    connections.append([])
+    pt1 = np.array((x[i],y[i]))
+    for v in range(num_of_devices):
+        if(v != i):
+            if(abs(x[i]-x[v])<=radius and abs(y[i]-y[v])<=radius):
+                pt2 = np.array((x[v],y[v]))
+                if(np.linalg.norm(pt1 - pt2) <= radius):
+                    connections[i].append(v)
+print(connections)
+
 while(True):
+    r = 50
     for i in range(num_of_devices):
-        r = 50
         if(status[i] == 1):
             plt.scatter(x[i],y[i],c="c",s=r**2)
             # plt.scatter(x[i],y[i],c="black")
-        else:
+    for i in range(num_of_devices):
+        if(status[i] == 0):
             plt.scatter(x[i],y[i],c="r")
             #pass
     plt.draw()
-    plt.pause(3)
+    plt.pause(2)
     checkstatus()
     plt.clf()
